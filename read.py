1d3da695cf63aa429191e4d4c7440b40ff7458d9
Cathy Chen <ingridlosna@gmail.com>
2021-02-13 16:30:10 +0800

    add filter

--- read.py
+++ read.py
@@ -10,3 +10,14 @@ for length in data:
 	sum_len += len(length)
 average = sum_len/(len(data))
 print('留言的平均長度為', average)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
+
+
+
