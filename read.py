0a461b6d62587f670792769324197c37cbfcea87
Cathy Chen <ingridlosna@gmail.com>
2021-06-26 18:14:12 +0800

    add word countfunction

--- read.py
+++ read.py
@@ -3,21 +3,71 @@ count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
-sum_len = 0
-for length in data:
-	sum_len += len(length)
-average = sum_len/(len(data))
-print('留言的平均長度為', average)
+# print(data[0])
 
-new = []
+
+wc = {} #wc=word count
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆留言長度小於100')
-print(new[0])
-print(new[1])
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增新的key進wc字典
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('請問你想查什麼字：')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為：', wc[word])
+	else:
+		print('這個字沒有出現過喔！')
+
+print('感謝使用本查詢功能')
 
 
 
+
+
+
+
+
+
+
+# sum_len = 0
+# for length in data:
+# 	sum_len += len(length)
+# average = sum_len/(len(data))
+# print('留言的平均長度為', average)
+
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('一共有', len(new), '筆留言長度小於100')
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言提到good')
+
+# good = [d for d in data if 'good' in d] #快清單寫法list comprehension，完全等於20~24
+
+# bad = ['bad' in d for d in data] #bad in d 為運算，即true or false
+
+# bad = []
+# for d in data:
+# 	bad.append('bad' in d)
